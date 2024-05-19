5bd8d13ecce73fccb2743ea124b5ad30cdb18427
AbdelilahChahbouni <abdelchahbouni@live.fr>
2024-05-19 13:50:06 +0100

    create functions view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_list_or_404
+from .models import Project , Tag
 
-# Create your views here.
+
+
+def home(request):
+    return render(request , "home.html")
+
+
+def project(request , id):
+    return render(request , "project.html")
+
+def contact(request):
+    return render(request , "contact.html")
+
+
+
+
+
